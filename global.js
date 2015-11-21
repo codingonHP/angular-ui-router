247fb742a277cc609954fa4c9f54abf00a11540f
codingonHP <vishal_24_anand@hotmail.com>
2015-11-21 19:45:17 +0530

    added hourly and 5 day forecast pages and routing configuration

--- global.js
+++ global.js
@@ -4,8 +4,12 @@
         .config(["$urlRouterProvider", "$stateProvider", function ($urlRouterProvider, $stateProvider) {
             $urlRouterProvider.otherwise('/home');
             $stateProvider.state('home', {
-                url: '/home',
-                templateUrl: '/templates/home.html'
-            });
+                    url: '/home',
+                    templateUrl: '/templates/Weather-hourly.html'
+                })
+                .state('rangeForecast', {
+                    url: '/rangeForecast',
+                    templateUrl: '/templates/weather-fivedays.html'
+                });
     }]);
 }());
